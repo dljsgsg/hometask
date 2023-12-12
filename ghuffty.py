f66538af837a8e8fb3622415aaa2290edce1077a
dljsgsg <153618446+dljsgsg@users.noreply.github.com>
2023-12-12 16:02:24 +0300

    Add files via upload

---
+++ ghuffty.py
@@ -0,0 +1,32 @@
+#1
+number = int(input("tell your number:"))
+if number %2==0:
+
+else:
+     print("this is odd number")
+else:
+    print("this is Edd Number")
+number = int(input("tell your number:"))
+#2
+if number %7==0:
+     print("Number is multiple 7")
+else:
+     print(" Number is not multiple 7")
+     #3
+a, b = int(input("number1:")), int(input("number2:"))
+if a > b:
+    print(a)
+else:
+    print(b)
+    #4
+a, b = int(input("number1:")), int(input("number2:"))
+if a < b:
+    print(a)
+else:
+     print(b)
+     #5
+a, b= int(input("number:")), int(input("number:"))
+print(f"сумма чисел: {a + b}")
+print(f"произведение чисел: {a * b}")
+print(f"разница чисел: {a // b}")
+print(f"среднее арифметическое:{a + b // 2}")
